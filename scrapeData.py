a2b597e1d262391b7b3b67af5d2c69b7eb02ef53
caglasozen <cagla.sozen@ug.bilkent.edu.tr>
2020-03-23 12:20:47 +0300

    Format fix

--- scrapeData.py
+++ scrapeData.py
@@ -14,7 +14,6 @@ print "Fetching Data for ", repo.full_name.upper() , "Repository... \n"
 #ISSUES INFO
 
 print "----Issues Data-----"
-
 all_issues = repo.get_issues(state='all')
 open_issues = repo.get_issues(state='open')
 closed_issues = repo.get_issues(state='closed')
@@ -24,15 +23,12 @@ print "Open Issue Count",open_issues.totalCount
 print "Closed Issue Count",closed_issues.totalCount, "\n"
 
 print "----Issue Comments-----"
-
 all_comments = repo.get_issues_comments()
-
 print "Issue Comment Count",all_comments.totalCount, "\n"
 
 
 #MILESTONES INFO
 print "----Milestones Data-----"
-
 all_milestones = repo.get_milestones(state='all')
 open_milestones = repo.get_milestones(state='open')
 closed_milestones = repo.get_milestones(state='closed')
@@ -53,77 +49,52 @@ print "Open Pull Request Count",open_pulls.totalCount
 print "Closed Pull Request Count",closed_pulls.totalCount, "\n"
 
 print "----Pull Request Comments-----"
-
 all_pull_comments = repo.get_pulls_comments()
-
 print "Pull Request Comment Count",all_pull_comments.totalCount, "\n"
 
 print "----Pull Request Review Comments-----"
-
 all_pull_review_comments = repo.get_pulls_review_comments()
-
 print "Pull Request Review Comment Count",all_pull_review_comments.totalCount, "\n"
-
 
 #BRANCH INFO
 print "----Branch Data-----"
-
 all_branches = repo.get_branches()
-
 print "Branch Count",all_branches.totalCount, "\n"
 
 #COMMENT INFO
 print "----Comment Data-----"
-
 all_comments = repo.get_comments()
-
 print "Comment Count",all_comments.totalCount, "\n"
 
 #COMMIT INFO
 print "----Commit Data-----"
-
 all_commits = repo.get_commits()
-
 print "Commit Count",all_commits.totalCount, "\n"
 
 #PROJECT INFO
-
 print "----Project Data-----"
-
 all_projects = repo.get_projects()
-
 print "Project Count",all_projects.totalCount, "\n"
 
 #CONTRIBUTOR INFO
-
 print "----Contributor Data-----"
-
 all_contributors = repo.get_contributors()
-
 print "Contributor Count",all_contributors.totalCount, "\n"
 
 #LABEL INFO
-
 print "----Label Data-----"
-
 all_labels = repo.get_labels()
-
 print "Label Count",all_labels.totalCount, "\n"
 
 #RELEASE INFO
-
 print "----Release Data-----"
-
 all_releases = repo.get_releases()
-
 print "Release Count",all_releases.totalCount, "\n"
 
 ###################################### STATS  #################################################
-#For further use
+#For later use
 
 contributor_stats = repo.get_stats_contributors()
-
 commit_stats = repo.get_stats_commit_activity()
-
 code_freq = repo.get_stats_code_frequency()
 
